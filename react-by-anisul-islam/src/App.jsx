83a5aac31093be70685c923a0377df01e47caeac
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-10-20 08:53:01 +0600

    css styling done

--- react-by-anisul-islam/src/App.jsx
+++ react-by-anisul-islam/src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const headingStyle = {
+  color:'red',
+  fontSize:'3rem'
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
-      
+      <h1 className='headingStyle'>Hello</h1>
+      <div className='card'>
+        <h3 className='cardTitle'>Call Family</h3>
+        <p className='cardDesc'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, quod.</p>
+        <p className='cardFooter'>Al Mahfuz</p>
+      </div>
       
     </>
   )
