236d6ba032e47c630ed4a8900a3549d95fbc310b
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-10-20 10:08:30 +0600

    array mapping done

--- react-by-anisul-islam/src/App.jsx
+++ react-by-anisul-islam/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Card from './Card'
+import Data from './data.json'
 import './App.css'
 
 const headingStyle = {
@@ -12,11 +13,13 @@ const headingStyle = {
 function App() {
   const [count, setCount] = useState(0)
 
+  let items = [];
+
   return (
     <>
       <h1 className='headingStyle'>Hello</h1>
-      <Card titleText="Call mother" descText="This is desc text 1"/>
-      <Card titleText="Call Father" descText="This is desc text 2"/>
+      {items = Data.map((item,index) => <Card key={index} titleText={item.title} descText={item.desc}/>)}
+
       
     </>
   )
