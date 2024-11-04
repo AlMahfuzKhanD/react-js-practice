e81ecf6887f0549603093bc13af6aa118baeec32
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-11-04 16:15:21 +0600

    nested mapping done

--- react-by-anisul-islam/src/App.jsx
+++ react-by-anisul-islam/src/App.jsx
@@ -13,12 +13,35 @@ const headingStyle = {
 function App() {
   const [count, setCount] = useState(0)
 
-  let items = [];
+  // let items = [];
+
+  // for(let x = 0; x <Data.length; x++){
+  //   items.push(<Card titleText={Data[x].title} descText={Data[x].desc}/>)
+  // }
 
   return (
     <>
       <h1 className='headingStyle'>Hello</h1>
-      {items = Data.map((item,index) => <Card key={index} titleText={item.title} descText={item.desc}/>)}
+      {/* {
+        Data.map((item,index) => (
+          <article key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.desc}</p>
+            {
+              item.phones.map((phone,index) => 
+                <div key={index}>
+                  <p> Home: {phone.home}</p>
+                  <p> Office: {phone.office}</p>
+                </div>
+              )
+            }
+          </article>
+
+        ))
+      } */}
+      {Data.map((item,index) => <Card key={index} titleText={item.title} descText={item.desc}/>)}
+      {/* {items = Data.map((item,index) => <Card key={index} titleText={item.title} descText={item.desc}/>)} */}
+      {/* {items} */}
 
       
     </>
