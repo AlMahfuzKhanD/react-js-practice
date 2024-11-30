a6217c22b9e2b4ef74f7d06ba04bafe44f7c99fa
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-11-30 23:54:29 +0600

    class 8 done code stoic

--- react-by-code-stoic/src/App.jsx
+++ react-by-code-stoic/src/App.jsx
@@ -1,16 +1,18 @@
-import Hello from './components/Hello'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Hello from "./components/Hello";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Hello/>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-by-code-stoic/src/components/Hello.jsx
+++ react-by-code-stoic/src/components/Hello.jsx
@@ -1,5 +1,5 @@
-function Hello(){
-    return <h1>Hello from a component!</h1>
+function Hello() {
+  return <h1>Hello from a component!</h1>;
 }
 
-export default Hello
+export default Hello;
