a82c6b14e2359c620d03a5ffddc70884d4a6c732
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-11-30 12:35:52 +0600

    react js code stoic started

---
+++ react-by-code-stoic/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from './components/Hello'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Hello/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-by-code-stoic/src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello(){
+    return <h1>Hello from a component!</h1>
+}
+
+export default Hello
