fd3ab9522767e5ee4e411016758bc2898265e2c3
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-01 12:40:41 +0600

    looping component done

---
+++ react-by-code-stoic/src/components/Fruit.jsx
@@ -0,0 +1,7 @@
+export default function Fruit({ name, price }) {
+  return (
+    <li>
+      {name} ${price}
+    </li>
+  );
+}
--- react-by-code-stoic/src/components/Fruits.jsx
+++ react-by-code-stoic/src/components/Fruits.jsx
@@ -1,10 +1,42 @@
+import Fruit from "./Fruit";
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana"];
+  //   const fruits = ["Apple", "Mango", "Banana"];
+  //   return (
+  //     <div>
+  //       <ul>
+  //         {fruits.map((item, index) => (
+  //           <li key={index}>{item}</li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   );
+  const fruits = [
+    {
+      name: "Apple",
+      price: 10,
+    },
+    {
+      name: "Mango",
+      price: 12,
+    },
+    {
+      name: "Banana",
+      price: 5,
+    },
+    {
+      name: "Orrange",
+      price: 7,
+    },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((item, index) => (
-          <li key={index}>{item}</li>
+          //   <li key={index}>
+          //     {item.name} ${item.price}
+          //   </li>
+
+          <Fruit key={index} name={item.name} price={item.price} />
         ))}
       </ul>
     </div>
