93b05ab4cdace989f58fa256525a6ae8514fc5ee
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-05 20:02:32 +0600

    multiple state practice done

--- react-by-code-stoic/src/components/Counter.jsx
+++ react-by-code-stoic/src/components/Counter.jsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(1);
   function increment() {
-    setCount(count + 1);
+    setCount(count + incrementBy);
   }
   function decrement() {
-    setCount(count - 1);
+    setCount(count - incrementBy);
+  }
+  function increaseIncrement() {
+    setIncrementBy(incrementBy + 1);
+  }
+  function decreaseIncrement() {
+    setIncrementBy(incrementBy - 1);
   }
   return (
     <div>
       <h1>Count value is: {count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+
+      <h1>We are incrementing the value by: {incrementBy} </h1>
+      <button onClick={increaseIncrement}>Increase increment</button>
+      <button onClick={decreaseIncrement}>Decrease increment</button>
     </div>
   );
 }
