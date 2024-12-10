1b93ff35d347f8e54665c42a2fea6942c16c95d5
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-10 20:20:19 +0600

    complete mark added to todo

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -3,10 +3,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    const newTodos = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newTodos);
+  }
+  const completeClass = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={completeClass} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span>
           <button
             onClick={() => handleDelete(item)}
