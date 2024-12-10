25811d0e24237cdd3abf7d7ad98f7462561c4651
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-10 20:58:52 +0600

    todo sort done on complete

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item, index) => (
+      {sortedTodos.map((item, index) => (
         <TodoItem key={index} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
