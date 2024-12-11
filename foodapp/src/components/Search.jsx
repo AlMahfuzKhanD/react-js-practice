8c885ac8f6c3a8842b6a65823efb49afd216f0a9
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-11 14:19:15 +0600

    relut fetched using api

---
+++ foodapp/src/components/Search.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "17a54c390f3c43c0a00ff326ab0b46fe";
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+}
