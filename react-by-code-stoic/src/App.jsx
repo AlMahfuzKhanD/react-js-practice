8eac837496e1481fc8d259881b75cfbc0664760b
AlMahfuzKhanD <almahfuz380@gmail.com>
2024-12-19 10:09:05 +0600

    router started

--- react-by-code-stoic/src/App.jsx
+++ react-by-code-stoic/src/App.jsx
@@ -1,37 +1,15 @@
-import Hello from "./components/Hello";
-import Fruits from "./components/Fruits";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import ConditionalComponent from "./components/ConditionalComponent";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
-import Form from "./components/Form";
-import UseReducer from "./components/UseReducer";
-import UseReducerObject from "./components/UseReducerObject";
-import UseComplexObjectReducer from "./components/UseComplexObjectReducer";
-import ReducerBankAccount from "./components/ReducerBankAccount";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // const seatNumbers = [1, 4, 7];
-  const person = {
-    name: "Rob",
-    message: "Hi",
-    seatNumbers: [1, 2, 3],
-  };
   return (
-    <div className="App">
-      {/* <Hello name="Mahfuz" message="Hello" seatNumbers={seatNumbers} /> */}
-      {/* <Hello person={person} /> */}
-      {/* <Hello name="Khan" message="Hi" /> */}
-      {/* <Form /> */}
-      {/* <UseReducer />
-      <UseReducerObject />
-      <UseComplexObjectReducer /> */}
-      {/* <ReducerBankAccount /> */}
-      This is the app Page
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ react-by-code-stoic/src/AppOld.jsx
@@ -0,0 +1,38 @@
+import Hello from "./components/Hello";
+import Fruits from "./components/Fruits";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ConditionalComponent from "./components/ConditionalComponent";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
+import UseReducer from "./components/UseReducer";
+import UseReducerObject from "./components/UseReducerObject";
+import UseComplexObjectReducer from "./components/UseComplexObjectReducer";
+import ReducerBankAccount from "./components/ReducerBankAccount";
+
+function App() {
+  const [count, setCount] = useState(0);
+  // const seatNumbers = [1, 4, 7];
+  const person = {
+    name: "Rob",
+    message: "Hi",
+    seatNumbers: [1, 2, 3],
+  };
+  return (
+    <div className="App">
+      {/* <Hello name="Mahfuz" message="Hello" seatNumbers={seatNumbers} /> */}
+      {/* <Hello person={person} /> */}
+      {/* <Hello name="Khan" message="Hi" /> */}
+      {/* <Form /> */}
+      {/* <UseReducer />
+      <UseReducerObject />
+      <UseComplexObjectReducer /> */}
+      {/* <ReducerBankAccount /> */}
+      This is the app Page
+    </div>
+  );
+}
+
+export default App;
